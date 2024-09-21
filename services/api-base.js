225e9fe8c0f3a8b5abb14b76b59d2f3f4a5d82c7
Harsh Jaiswani <harshjaiswani2003@gmail.com>
2024-09-21 20:39:37 +0530

    feat: updated api-base file

--- services/api-base.js
+++ services/api-base.js
@@ -4,25 +4,25 @@ import Cookies from "js-cookie";
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-const setAccessToken = async (accessToken) => {
+export const setAccessToken = async (accessToken) => {
   Cookies.set("accessToken", accessToken, {
     expires: 7, // days
   });
 };
 
-const setRefreshToken = async (refreshToken) => {
+export const setRefreshToken = async (refreshToken) => {
   Cookies.set("refreshToken", refreshToken, {
     expires: 7 * 4, // days
   });
 };
 
-const deleteAuthCookies = async () => {
+export const deleteAuthCookies = async () => {
   Cookies.remove("accessToken");
   Cookies.remove("refreshToken");
-  return { success: "Cookies Cleared" };
+  return { success: true, message: "Cookies Cleared" };
 };
 
-const getAccessToken = async () => {
+export const getAccessToken = async () => {
   let cookie = Cookies.get("accessToken");
   if (!cookie) {
     let refreshCookie = await getRefreshToken();
@@ -31,19 +31,19 @@ const getAccessToken = async () => {
   return cookie;
 };
 
-const getRefreshToken = async () => {
+export const getRefreshToken = async () => {
   return Cookies.get("refreshToken");
 };
 
-const refreshToken = async (refreshCookie) => {
+export const refreshToken = async (refreshCookie) => {
   const response = await ApiBase.post("/api/token/refresh/", {
     refresh_token: refreshCookie,
   });
   await setAccessToken(response.access_token);
-  return response.access_token;
+  return response.access_token ?? null;
 };
 
-const get = async (url, query = {}) => {
+export const get = async (url, query = {}) => {
   let headers = {};
 
   let token = await getAccessToken();
@@ -59,14 +59,14 @@ const get = async (url, query = {}) => {
       params: query,
       headers,
     });
-    return response.data;
+    return { success: true, data: response.data };
   } catch (error) {
     console.log(error);
-    return error.response.data;
+    return { success: false, message: error.response.data };
   }
 };
 
-const post = async (url, data, _headers = {}) => {
+export const post = async (url, data, _headers = {}) => {
   let headers = {};
 
   let token = await getAccessToken();
@@ -82,36 +82,36 @@ const post = async (url, data, _headers = {}) => {
     const response = await axios.post(`${baseUrl}${url}`, data, {
       headers,
     });
-    return response.data;
+    return { success: true, data: response.data };
   } catch (error) {
     console.log(error);
-    return error.response.data;
+    return { success: false, message: error.response.data };
   }
 };
 
-const getWithoutHeader = async (url, query = {}) => {
+export const getWithoutHeader = async (url, query = {}) => {
   try {
     const response = await axios.get(`${baseUrl}${url}`, {
       params: query,
     });
-    return response.data;
+    return { success: true, data: response.data };
   } catch (error) {
     console.log(error);
-    return error.response.data;
+    return { success: false, message: error.response.data };
   }
 };
 
-const postWithoutHeader = async (url, data) => {
+export const postWithoutHeader = async (url, data) => {
   try {
     const response = await axios.post(`${baseUrl}${url}`, data);
-    return response.data;
+    return { success: true, data: response.data };
   } catch (error) {
     console.log(error);
-    return error.response.data;
+    return { success: false, message: error.response.data };
   }
 };
 
-const patch = async (url, data) => {
+export const patch = async (url, data) => {
   let headers = {};
 
   let token = await getAccessToken();
@@ -126,14 +126,14 @@ const patch = async (url, data) => {
     const response = await axios.patch(`${baseUrl}${url}`, data, {
       headers,
     });
-    return response.data;
+    return { success: true, data: response.data };
   } catch (error) {
     console.log(error);
-    return error.response.data;
+    return { success: false, message: error.response.data };
   }
 };
 
-const erase = async (url) => {
+export const erase = async (url) => {
   let headers = {};
 
   let token = await getAccessToken();
@@ -148,25 +148,9 @@ const erase = async (url) => {
     const response = await axios.delete(`${baseUrl}${url}`, {
       headers,
     });
-    return response.data;
+    return { success: true, data: response.data };
   } catch (error) {
     console.log(error);
-    return error.response.data;
+    return { success: false, message: error.response.data };
   }
 };
-
-const ApiBase = {
-  setAccessToken,
-  getAccessToken,
-  getRefreshToken,
-  setRefreshToken,
-  deleteAuthCookies,
-  get,
-  post,
-  getWithoutHeader,
-  postWithoutHeader,
-  patch,
-  erase,
-};
-
-export default ApiBase;
